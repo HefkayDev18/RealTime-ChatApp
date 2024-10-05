5edb7c09c6b6dbe9ca9172358de2b33fef9f39a9
hefkay <hefkay.lawal2019@gmail.com>
2024-10-04 21:36:05 -0700

    changed chatbox

--- chatapp-frontend/src/components/Chatbox.jsx
+++ chatapp-frontend/src/components/Chatbox.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, Input, Button, HStack, VStack, Text, Avatar, Heading, Flex } from "@chakra-ui/react";
 import { io } from "socket.io-client";
-import FormatTime from "../functions/formatTime";
+import FormatTime from "../functions/FormatTime";
 
 const socket = io("http://localhost:4000");
 
